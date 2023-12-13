90f35b3ae3cf2d0e7032ede820a882b31330cac3
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-12-13 20:13:02 +0530

    updated

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,16 +1,15 @@
-import { forwardRef, useImperativeHandle, useRef, useContext } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import Cart from './Cart.jsx';
-import { CartContext } from '../store/shopping-cart-context.jsx';
+
 
 const CartModal = forwardRef(function Modal(
-  // { cartItems, onUpdateCartItemQuantity, title, actions },
   { title, actions },
   ref
 ) {
   const dialog = useRef();
 
-  const { items, updateItemQuantity } = useContext(CartContext);
+
   useImperativeHandle(ref, () => {
     return {
       open: () => {
